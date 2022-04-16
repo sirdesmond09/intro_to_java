fd306b98d176c2797598c0edbb7594f0ba2d4bf3
sirdesmond09 <sirdesmond09@gmail.com>
2022-04-16 17:48:14 +0100

    gui programming

---
+++ GUI_PROJ/my_proj/src/application/Lunch.java
@@ -0,0 +1,13 @@
+package application;
+
+abstract class Food {
+    void printFlavor() {}
+}
+
+class Pepper extends Food {}
+	public class Lunch {
+	    public static void main(String[] args) {
+	        Food lunch = new Pepper();
+	        lunch.printFlavor();
+	    }
+}
